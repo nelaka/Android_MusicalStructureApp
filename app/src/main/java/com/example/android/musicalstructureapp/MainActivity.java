90cac8dd8992de5f8305a5434715a05fea12762e
nelaka <ekalkopo@gmail.com>
2017-05-12 00:04:49 +0300

    version 1 of project (in progress)

--- app/src/main/java/com/example/android/musicalstructureapp/MainActivity.java
+++ app/src/main/java/com/example/android/musicalstructureapp/MainActivity.java
@@ -1,13 +1,46 @@
 package com.example.android.musicalstructureapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
+
+        //Find the View that shows the number category
+        TextView songs = (TextView) findViewById(R.id.songs);
+        // Set a clickListener on that View
+        songs.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                //Create a new intent to open the {@link NumberActivity}
+                Intent songsIntent = new Intent(MainActivity.this, SongsActivity.class);
+                //Start the new activity
+                startActivity(songsIntent);
+            }
+        });
+
+        //Find the View that shows the number category
+        TextView albums = (TextView) findViewById(R.id.albums);
+        // Set a clickListener on that View
+        albums.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                //Create a new intent to open the {@link NumberActivity}
+                Intent albumsIntent = new Intent(MainActivity.this, AlbumsActivity.class);
+                //Start the new activity
+                startActivity(albumsIntent);
+            }
+        });
     }
 }
